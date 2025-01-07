5e4df747dd8de48d1a31da931312864c6648f0e4
dimanaStoyanova7 <stoyanovadimana557@gmail.com>
2025-01-07 18:55:01 +0200

    Time: 1 ms (99.81%) | Memory: 52.9 MB (64.18%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,6 +3,8 @@ class Solution {
         //brute force: o(n^2)
 
         //hashmap o(n) space, o(n) time
+
+        /*
         Map<Integer, Integer> map = new HashMap<>();
         int majorityCount = nums.length /2;
         for(int num : nums){
@@ -10,6 +12,19 @@ class Solution {
             if(map.get(num)>majorityCount) return num;
         }
         return -1;
+
+        */
+
+        // voting algorithms o(1) space, o(n) time
+        int count = 0;
+        int candidate =0;
+        for(int num : nums){
+            if(count == 0) candidate = num;
+            if(candidate == num){
+                count++;
+            } else count--;
+        }
+        return candidate;
     
     }
 }
