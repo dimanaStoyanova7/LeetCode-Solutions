a290c527d65ff5cad4099c8fef56964debbccc3c
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-01-25 13:16:17 +0200

    Time: 19 ms (59.04%) | Memory: 45.1 MB (46.78%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,6 +2,7 @@ class Solution {
     
     public String longestPalindrome(String s) {
         //brute force
+        /*
         if(s.length() < 2) return s;
         String res = "";
         for(int i=0; i<s.length();i++){
@@ -11,6 +12,22 @@ class Solution {
             }
         }
         return res;
+        */
+        String longest = "";
+        for(int i=0; i<s.length(); i++){
+            String even = expandFromCentre(s, i,i);
+            String odd = expandFromCentre(s, i, i+1);
+            if(even.length()>longest.length()) longest = even;
+            if(odd.length()>longest.length()) longest = odd;
+        }
+        return longest;
+    }
+    String expandFromCentre(String s, int l, int r){
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+        }
+        return s.substring(l+1, r);
     }
     boolean isPalindrome(String s){
         int left = 0;
