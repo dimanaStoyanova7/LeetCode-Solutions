9b1bab427b754492bcccd1607b92f00b4983bc85
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-01-26 19:44:09 +0200

    Time: 23 ms (92.01%) | Memory: 47 MB (15.98%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -3,6 +3,7 @@ class Solution {
         //work with prefix sums
         //at each prefix sum, check if the value of prefix sum - k was already a prefix sum,
         // and if yes, how many times
+    
         Map<Integer, Integer> prefixSumCounts = new HashMap<>();
         prefixSumCounts.put(0,1);
         int res = 0;
