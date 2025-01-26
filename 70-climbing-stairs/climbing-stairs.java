52ad2a28eb8d1ff86e00a58d08395a6012e59b30
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-01-26 14:10:06 +0200

    Time: 0 ms (100.00%) | Memory: 40.1 MB (95.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
+        //recursive fibonacci
+/*
+        if(n==1) return 1;
+        if(n==2) return 2;
+        return climbStairs(n-1)+ climbStairs(n-2);
+*/
         //dp with o(n) space complexity
         //formula dp[i] = dp[i-1]+dp[i-2]
 
+        
         if(n==0) return 0;
         if(n==1) return 1;
         if(n==2) return 2;
         int[] dp = new int[n+1];
-        dp[0]=0;
+        dp[0]=1;
         dp[1]=1;
-        dp[2]=2;
-        for(int i=3; i<=n; i++){
+        //dp[2]=2;
+        for(int i=2; i<=n; i++){
             dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[n];
-
+  
 
         //dp approach with constant space complexity
         /*
