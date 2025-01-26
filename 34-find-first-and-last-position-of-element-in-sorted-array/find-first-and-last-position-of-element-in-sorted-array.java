aa965970b23669e6748c141af498094ba075f08c
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-01-26 22:28:27 +0200

    Time: 0 ms (100.00%) | Memory: 46 MB (47.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        // brute force approach:  iterate from beggining and track
-        // optimizer approah : o(log(n)) - 2 binary searches
-        int l1 = 0;
-        int r1 = nums.length-1;
-        int l2 = 0;
-        int r2 = nums.length-1;
-        int[] result = {-1,-1};
-        while(l1<=r1){
-            int mid = l1+(r1-l1)/2;
-            if(nums[mid]<target) l1 = mid + 1;
-            else r1 = mid-1; 
+        return new int[]{findFirst(nums, target), findLast(nums, target)};
+    }
+    int findFirst(int[] nums, int target){
+        int res = -1;
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low) /2;
+            if (nums[mid]==target){
+                res = mid;
+                high = mid-1;
+            } else if (nums[mid] < target) {
+            low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
         }
-        while(l2<=r2){
-            int mid = l2+(r2-l2)/2;
-            if(nums[mid]<=target) l2 = mid+1;
-            else r2 = mid-1;
+        return res;
+
+    }
+    int findLast(int[] nums, int target){
+        int res = -1;
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low) /2;
+            if (nums[mid]==target){
+                res = mid;
+                low = mid+1;
+            } else if (nums[mid] < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
         }
-        if (l1 < nums.length && nums[l1] == target) {
-            result[0] = l1;
-        }
-        if (r2 >= 0 && nums[r2] == target) {
-            result[1] = r2;
-        }
-        return result;
+        return res;
     }
 }
