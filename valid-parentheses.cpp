6e860b07aa0cb2e47a6f83ff707dec63d74df47e
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-01-26 15:19:27 +0200

    Add valid-parentheses.cpp

---
+++ valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length()==0) return true;
+        Stack<Character> stack = new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(stack.isEmpty()) stack.push(c);
+            else if(c=='}' && stack.peek()=='{') stack.pop();
+            else if(c==')' && stack.peek()=='(') stack.pop();
+            else if(c==']' && stack.peek()=='[') stack.pop();   
+            else stack.push(c);
+
+        }
+        return stack.isEmpty();
+    }
+}
