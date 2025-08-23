cdd99733692df8259c6f2edf54b27f0aaa1b81d0
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-08-23 22:08:02 +0300

    Time: 0 ms (100.00%) | Memory: 49.5 MB (20.73%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,9 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-        for(int i=0; i<s.length/2; i++){
-            char temp = s[i];
-            s[i] = s[s.length-i-1];
-            s[s.length-i-1]=temp;
+        // for(int i=0; i<s.length/2; i++){
+        //     char temp = s[i];
+        //     s[i] = s[s.length-i-1];
+        //     s[s.length-i-1]=temp;
+        // }
+        // two pointers approach
+        int l = 0;
+        int r = s.length -1;
+        while(r>l){
+            char temp = s[l];
+            s[l++]=s[r];
+            s[r--]=temp;
         }
     }
 }
