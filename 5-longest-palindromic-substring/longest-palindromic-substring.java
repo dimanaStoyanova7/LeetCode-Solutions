9f4dfbd2d9fee1e88de67d812eccf64a3a95d4ea
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-08-27 23:02:36 +0300

    Time: 15 ms (85.60%) | Memory: 42.9 MB (53.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,26 @@
 class Solution {
-    
     public String longestPalindrome(String s) {
-        //brute force
-        /*
-        if(s.length() < 2) return s;
-        String res = "";
-        for(int i=0; i<s.length();i++){
-            for(int j=i; j<s.length(); j++){
-                String current = s.substring(i, j+1);
-                if(isPalindrome(current) && current.length()>res.length()) res = current;
+        int[] res = new int[] {0,0};
+        for(int i = 0; i<s.length(); i++){
+            int oddLength = expand(s, i, i);
+            int evenLength = expand(s,i, i+1);
+            if(oddLength > res[1] - res[0]+1){
+                res[0] = i - oddLength/2;
+                res [1] = i + oddLength/2;
+            }
+            if(evenLength > res[1] - res[0]+1){
+                res[0] = i - evenLength/2 + 1;
+                res[1] = i + (evenLength/2) ;
+
             }
         }
-        return res;
-        */
-        String longest = "";
-        for(int i=0; i<s.length(); i++){
-            String even = expandFromCentre(s, i,i);
-            String odd = expandFromCentre(s, i, i+1);
-            if(even.length()>longest.length()) longest = even;
-            if(odd.length()>longest.length()) longest = odd;
-        }
-        return longest;
+        return s.substring(res[0], res[1]+1);
     }
-    String expandFromCentre(String s, int l, int r){
-        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+    public int expand(String s, int l, int r){
+        while(l>=0 && r< s.length() && s.charAt(l)==s.charAt(r)){
             l--;
             r++;
         }
-        return s.substring(l+1, r);
-    }
-    boolean isPalindrome(String s){
-        int left = 0;
-        int right = s.length()-1;
-        while(left<=right){
-            if(s.charAt(left)!=s.charAt(right)) return false;
-            left++;
-            right--;
-        }
-        return true;
+        return r-l-1;
     }
 }
