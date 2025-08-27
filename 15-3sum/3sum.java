e37a333b2037f65d32c3634fdaa85fd539c0f737
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-08-27 23:43:13 +0300

    Time: 30 ms (70.37%) | Memory: 52.1 MB (29.86%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,61 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums); 
+        Arrays.sort(nums);
         for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int curr = nums[i];
-            if(i>0 && curr==nums[i-1]) continue;
-            //use two pointers and implement two sum
             int l = i+1;
-            int r = nums.length-1;
-            while (l<r){
-                int sum = nums[l]+nums[r]+curr;
+            int r = nums.length - 1;
+            while(l<r){
+                int sum = curr+nums[l]+nums[r];
                 if(sum==0){
                     res.add(Arrays.asList(curr, nums[l], nums[r]));
-                    l++; r--;
-                    //skip dublicates on the right and left
-                    while (l < r && nums[l]==nums[l-1]) l++;
-                    while (l < r && nums[r]==nums[r+1]) r--;
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1])l++;
+                    while(l<r && nums[r]==nums[r+1])r--;
                 }
-                else if (sum>0) r--;
-                else l++;
+                else if(sum>0){
+                    r--;
+                }
+                else if(sum<0){
+                    l++;
+                }
             }
-            
         }
         return res;
+
+
+
+
+
+
+
+
+
+        // List<List<Integer>> res = new ArrayList<>();
+        // Arrays.sort(nums); 
+        // for(int i=0; i<nums.length-2; i++){
+        //     int curr = nums[i];
+        //     if(i>0 && curr==nums[i-1]) continue;
+        //     //use two pointers and implement two sum
+        //     int l = i+1;
+        //     int r = nums.length-1;
+        //     while (l<r){
+        //         int sum = nums[l]+nums[r]+curr;
+        //         if(sum==0){
+        //             res.add(Arrays.asList(curr, nums[l], nums[r]));
+        //             l++; r--;
+        //             //skip dublicates on the right and left
+        //             while (l < r && nums[l]==nums[l-1]) l++;
+        //             while (l < r && nums[r]==nums[r+1]) r--;
+        //         }
+        //         else if (sum>0) r--;
+        //         else l++;
+        //     }
+            
+        // }
+        // return res;
     }
 }
