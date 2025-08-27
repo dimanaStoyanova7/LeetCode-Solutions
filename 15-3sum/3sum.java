2b0587b052a950a7debbe89f0743c74544ea4941
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-08-27 23:37:13 +0300

    Time: 29 ms (74.52%) | Memory: 51.5 MB (57.14%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,52 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        /*
-        Set<List<Integer>> result = new HashSet<>();
-        Arrays.sort(nums);
-        for (int i = 0; i<nums.length-2; i++){
-            int target = -nums[i];
-            Set<Integer> seen = new HashSet<>();
-            for(int j = i+1; j<nums.length; j++){
-                int complement  = target -nums[j];
-                if(seen.contains(complement)){
-                    result.add(Arrays.asList(nums[i], nums[j], complement));
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); 
+        for(int i=0; i<nums.length-2; i++){
+            int curr = nums[i];
+            if(i>0 && curr==nums[i-1]) continue;
+            //use two pointers and implement two sum
+            int l = i+1;
+            int r = nums.length-1;
+            while (l<r){
+                int sum = nums[l]+nums[r]+curr;
+                if(sum==0){
+                    res.add(Arrays.asList(curr, nums[l], nums[r]));
+                    l++; r--;
+                    //skip dublicates on the right and left
+                    while (l < r && nums[l]==nums[l-1]) l++;
+                    while (l < r && nums[r]==nums[r+1]) r--;
                 }
-                seen.add(nums[j]);
+                else if (sum>0) r--;
+                else l++;
             }
-        }
-        return new ArrayList<>(result);
-        */
-        Set<List<Integer>> result = new HashSet<>();
-        
-        // Step 1: Sort the array
-        Arrays.sort(nums);
-        
-        // Step 2: Iterate through the array
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int target = -nums[i]; // Target for the two-sum
-            int left = i + 1;
-            int right = nums.length - 1;
             
-            // Step 3: Two-pointer approach
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                
-                if (sum == target) {
-                    // Add triplet to the set (automatically handles duplicates)
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++; // Increase the left pointer if the sum is too small
-                } else {
-                    right--; // Decrease the right pointer if the sum is too large
-                }
-            }
         }
-        
-        return new ArrayList<>(result);
+        return res;
     }
 }
