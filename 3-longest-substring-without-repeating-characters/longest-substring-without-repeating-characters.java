36709323fba1ce5b7e6d8be7394256102d29b2ea
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-09-11 10:21:24 +0300

    Time: 6 ms (72.28%) | Memory: 44.8 MB (58.09%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -9,11 +9,10 @@ class Solution {
             char curr =  s.charAt(right);
             //if char is in the set: move left while char[left] is thethe current char and once more
             if(chars.contains(curr)){
-                while(s.charAt(left)!=curr){
+                while(chars.contains(curr)){
                     chars.remove(s.charAt(left));
                     left++;
                 }
-                left++;
             }
             //if char is not in the set: add it, update currLength, possibly maxLen
             chars.add(curr);
