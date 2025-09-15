9d65f66a27b21a0cc29d2e669850fe4cd6b74dc9
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-09-15 20:55:35 +0300

    Time: 26 ms (36.31%) | Memory: 57.4 MB (85.74%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int n = nums.length;
-        if (k == 0) { // need sum 0 with length >= 2 -> two consecutive zeros
-            for (int i = 1; i < n; i++) if (nums[i] == 0 && nums[i - 1] == 0) return true;
-            return false;
-        }
-        if (k < 0) k = -k; // optional normalize
+         int n = nums.length;
+        // if (k == 0) { // need sum 0 with length >= 2 -> two consecutive zeros
+        //     for (int i = 1; i < n; i++) if (nums[i] == 0 && nums[i - 1] == 0) return true;
+        //     return false;
+        // }
+        // if (k < 0) k = -k; // optional normalize
 
         Map<Integer, Integer> firstIndex = new HashMap<>();
         firstIndex.put(0, -1); // remainder 0 at index -1
