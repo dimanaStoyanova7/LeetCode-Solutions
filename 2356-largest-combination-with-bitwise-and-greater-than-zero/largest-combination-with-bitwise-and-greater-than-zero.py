7007cd6fe8d27ef151cf330155110e98638f8c79
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2025-10-17 02:07:28 +0300

    Time: 982 ms (31.25%) | Memory: 18.7 MB (87.50%) - LeetSync

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestCombination(self, candidates):
+        """
+        :type candidates: List[int]
+        :rtype: int
+        """
+        if not candidates:
+            return 0
+
+        limit_bits = max(candidates).bit_length()
+        res = 0
+
+        for b in range(limit_bits):
+            mask = 1 << b
+            cnt = 0
+            for x in candidates:
+                if x & mask:
+                    cnt+=1
+                res = max(res, cnt)
+        return res
+        
