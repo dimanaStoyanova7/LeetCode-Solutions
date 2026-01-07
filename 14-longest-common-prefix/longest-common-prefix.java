1463455e2d9074cf43db517cf79b9ec9bca81355
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2026-01-07 19:59:04 +0100

    Time: 1 ms (64.58%) | Memory: 43.3 MB (50.84%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,29 +1,33 @@
 class Solution {
+
+
     public String longestCommonPrefix(String[] strs) {
-        //horizontal scanning
-        /*
-        if(strs.length == 0 || strs == null) return "";
-        String prefix = strs[0];
-        for (int i =1; i<strs.length; i++) {
-            while(strs[i].indexOf(prefix)!=0){
-                prefix = prefix.substring(0, prefix.length()-1);
-                if(prefix.isEmpty()) return "";
-            }
+        //stringbuilder as a result
+        //append to stringbuilder if all strs at index i have the same character
+        if (strs.length == 0) return "";
+        StringBuilder sb = new StringBuilder();
+
+        int minLen = Integer.MAX_VALUE;
+        for (String s : strs) {
+            minLen = Math.min(minLen, s.length());
         }
-        return prefix;
-        */
-        //vertical scanning 
-        if(strs.length == 0 || strs == null) return "";
-        for(int i = 0; i<strs[0].length(); i++){
+
+        for(int i = 0; i< minLen; i++){
+            boolean areTheSame = true;
             char c = strs[0].charAt(i);
-            for(int j=1; j<strs.length; j++){
-                if(i==strs[j].length() || strs[j].charAt(i) != c){
-                    return strs[0].substring(0, i);
+            for(String str: strs){
+                if (str.charAt(i) != c){
+                    areTheSame = false;
                 }
             }
-
+            if(areTheSame){
+                sb.append(c);
+            }
+            else{
+                return sb.toString();
+            }
         }
-        return strs[0];
-
+        return sb.toString();
+        
     }
 }
