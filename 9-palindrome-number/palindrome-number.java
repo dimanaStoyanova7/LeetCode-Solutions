4cb9b504b2d1c62bcab1da44f150704c7607664a
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2026-01-07 19:20:57 +0100

    Time: 5 ms (73.20%) | Memory: 46.2 MB (8.69%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        //if negative -> false
-        if(x<0){
+        //if negative: return false
+        if (x<0){
             return false;
         }
-        //revert number
+        int copyX = x;
         int reverted = 0;
-        int original = x;
         while(x>0){
-            reverted = reverted*10 + (x%10);
+            reverted = reverted*10 + x%10;
             x /= 10;
         }
-        //compare reverted and x
-        return reverted == original;
+        return reverted == copyX;
     }
 }
