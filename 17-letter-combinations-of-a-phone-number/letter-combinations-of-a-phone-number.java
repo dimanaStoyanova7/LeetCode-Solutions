597fc4d84a0ba1f333cbd3ef8286707f512edf1d
Dimana Stoyanova <stoyanovadimana557@gmail.com>
2026-09-20 11:18:16 +0200

    Time: 3 ms (36.75%) | Memory: 48.9 MB (22.21%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,22 +1,38 @@
 class Solution {
-    public String[] mapping = new String[]{"","","abc","def", "ghi","jkl", "mno", "pqrs","tuv","wxyz"};
-
     public List<String> letterCombinations(String digits) {
-        if(digits.length()==0) return List.of();
-        List<String> res = new ArrayList<>();
-        backtrack(res, digits, new StringBuilder(""), 0);
-        return res;
+        //have a map
+        Map<Character, String> map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        //call a backtracking method to compose the combinations
+        List<String> result = new ArrayList<>();
+        addCombinations(result, "", 0,digits, map);
+        return result;
     }
-    void backtrack(List<String> result, String digits, StringBuilder current, int index){
+    /// current string
+    // index from the digits to take next
+    /// digits to iterate though
+    /// map to take from
+    private void addCombinations(List<String> result, String current, int index, String digits, Map<Character, String> map){
         if(current.length()==digits.length()){
-            result.add(current.toString());
+            result.add(current);
             return;
         }
-        String currentLetters = mapping[digits.charAt(index)-'0'];
-        for(char c : currentLetters.toCharArray()){
-            current.append(c);
-            backtrack(result, digits, current, index+1);
-            current.deleteCharAt(current.length()-1);
+        //take the digit at inde
+        char digit = digits.charAt(index);
+        //get the possible letters for that digit
+        String letters = map.get(digit);
+        //append 
+        //recursive call
+        //unappend
+        for(char letter : letters.toCharArray()){
+            addCombinations(result,current+letter, index+1, digits, map);
         }
     }
 }
